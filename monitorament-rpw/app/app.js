67c6857d6713ba479b88657a5b2515e0f80dafba
BOML <bruno.melo@gft.com>
2016-09-19 17:20:56 -0300

    Rewrite role

--- monitorament-rpw/app/app.js
+++ monitorament-rpw/app/app.js
@@ -27,7 +27,7 @@ var app = angular.module('app',['ui.router',
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider)
 {
    	// remove o # da url
-   	$locationProvider.html5Mode(true);
+   	//$locationProvider.html5Mode(true);
  
    	$urlRouterProvider.otherwise('/');
 
@@ -117,5 +117,5 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider)
     });
 
 	    // Utilizando o HTML5 History API
-	$locationProvider.html5Mode(true);
+	 $locationProvider.html5Mode(true);
 });
